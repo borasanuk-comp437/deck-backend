7198c619a71343f9ba69b8272b3581d0d461ad0f
borasanuk <bora@aeontech.io>
2022-05-18 19:23:42 +0300

    review based recommendation

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+import numpy as np
 import json
 import random
 import requests
 import urllib
 from keys import API_KEY
+import engine
 
 app = Flask(__name__)
 
@@ -39,8 +41,8 @@ def randomSuggestions():
     return response
 
 
-@app.route("/suggest")
-def suggest():
+@app.route("/simplesuggest")
+def simplesuggest():
     query = str(request.args['query'])
     exclude = str(request.args['exclude'])
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
@@ -90,3 +92,65 @@ def citySearch():
     response = jsonify(data)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+
+@app.route("/suggest")
+def suggest():
+    # query = str(request.args['query'])
+    # exclude = str(request.args['exclude'])
+    # base_id = str(request.args['base_id'])
+    query = "48.8584,2.2945"
+    exclude = []
+    base_id = "ChIJbWdK0eZv5kcROmLZY2qD4gk"
+    base_text = get_reviews(base_id)
+    base = [base_id, base_text]
+    candidates = get_suggestion_candidates(query, exclude)
+    embedded_candidates = []
+    for candidate in candidates:
+        candidate_id = candidate["place_id"]
+        candidate_text = get_reviews(candidate_id)
+        embedded_candidates.append([candidate_id, candidate_text])
+    recommendations = engine.get_recommendations(
+        np.array(base), np.array(embedded_candidates))
+    recommendations = recommendations[:5,:]
+    print(recommendations[:,1])
+    candidates = [c for c in candidates if c["place_id"] in recommendations[:,1]]
+    response = jsonify(candidates)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+def get_reviews(place_id):
+    # place_id = "ChIJbWdK0eZv5kcROmLZY2qD4gk"
+    url = "https://maps.googleapis.com/maps/api/place/details/json?"
+    params = {"key": API_KEY, "place_id": place_id,
+              "fields": "name,type,reviews"}
+    url_params = urllib.parse.urlencode(params)
+    url = f"{url}{url_params}"
+    payload = {}
+    headers = {}
+    data = requests.request("GET", url, headers=headers, data=payload).json()
+    types = data["result"]["types"]
+    name = data["result"]["name"]
+    reviews = data["result"]["reviews"]
+    reviews = [reviews[i]["text"] for i in range(len(reviews))]
+    embedded_reviews = name + " "
+    for t in types:
+        embedded_reviews += t + " "
+    for review in reviews:
+        embedded_reviews += review
+    return embedded_reviews
+
+
+def get_suggestion_candidates(query, exclude):
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    params = {"location": query,
+              "key": API_KEY, "radius": 15000, "type": "tourist_attraction"}
+    url_params = urllib.parse.urlencode(params)
+    url = f"{url}{url_params}"
+    payload = {}
+    headers = {}
+    data = requests.request("GET", url, headers=headers, data=payload)
+    candidates = [p for p in data.json()["results"]
+                  if p["place_id"] not in exclude]
+    return candidates
--- engine.py
+++ engine.py
@@ -6,12 +6,11 @@ model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
 def get_recommendations(base, candidates):
   print("started")
-  base_text = base[:,1]
+  base_text = base[1]
   candidates_ids = candidates[:,0]
   candidates_text = candidates[:,1]
   base_emb = np.array(model.encode(base_text, show_progress_bar=True))
   candidates_emb = np.array(model.encode(candidates_text, show_progress_bar=True))
-  cs = cosine_similarity(base_emb, candidates_emb)
+  cs = cosine_similarity(base_emb.reshape(1, -1), candidates_emb)
   recommendations = sorted(zip(cs[0], candidates_ids), reverse=True)
-  print(recommendations)
-  return recommendations
+  return np.array(recommendations)
